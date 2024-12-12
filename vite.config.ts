eb43899d006feb1e0b3ac02434a81552672fe9b6
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-12 07:02:18 -0600

    config vite for githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/web-anasty/', // Reemplaza <REPO_NAME> con el nombre de tu repositorio
   plugins: [vue()],
   build: {
     outDir: 'dist',
